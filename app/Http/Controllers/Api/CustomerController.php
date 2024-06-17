3b9cf07d1cc7f786333bb06aedfe1d90407ed08a
caessarrr <saintcaessar@gmail.com>
2024-06-17 09:12:56 +0700

    add

---
+++ app/Http/Controllers/Api/CustomerController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Customer;
+use Illuminate\Http\Request;
+
+class CustomerController extends Controller
+{
+    public function index()
+    {
+        $customers = Customer::all();
+        return response()->json($customers);
+    }
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:customers',
+            'phone' => 'nullable|string',
+            'address' => 'nullable|string'
+        ]);
+
+        $customer = Customer::create($validatedData);
+        return response()->json($customer, 201);
+    }
+
+    public function show(Customer $customer)
+    {
+        return response()->json($customer);
+    }
+
+    public function update(Request $request, Customer $customer)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:customers,email,'.$customer->id,
+            'phone' => 'nullable|string',
+            'address' => 'nullable|string'
+        ]);
+
+        $customer->update($validatedData);
+        return response()->json($customer);
+    }
+
+    public function destroy(Customer $customer)
+    {
+        $customer->delete();
+        return response()->json(null, 204);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,21 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+use App\Http\Controllers\Api\CustomerController;
+
+// Mendapatkan semua customer
+Route::get('/customers', [CustomerController::class, 'index']);
+
+// Mendapatkan customer berdasarkan ID
+Route::get('/customers/{customer}', [CustomerController::class, 'show']);
+
+// Membuat customer baru
+Route::post('/customers', [CustomerController::class, 'store']);
+
+// Memperbarui customer berdasarkan ID
+Route::put('/customers/{customer}', [CustomerController::class, 'update']);
+
+// Menghapus customer berdasarkan ID
+Route::delete('/customers/{customer}', [CustomerController::class, 'destroy']);
+
